48df148e96bee5ed5be6223ae27ef75a93b834a6
ahmed smiai <ahmedsmiai3@gmail.com>
2023-03-25 01:49:07 +0100

    spring project

---
+++ src/main/java/esprit/etudiant/tn/healthcloud/HealthCloudApplication.java
@@ -0,0 +1,13 @@
+package esprit.etudiant.tn.healthcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esprit/etudiant/tn/healthcloud/HealthCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.etudiant.tn.healthcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
